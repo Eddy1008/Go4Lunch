6c5235dcbe065733b2e7545554dd46da15c5de08
Eddy1008 <galmandeddy@gmail.com>
2023-05-01 22:24:22 +0200

    NavigationDrawer YOUR LUNCH redirection vers restaurantActivité

--- app/src/main/java/fr/zante/go4lunch/MainActivity.java
+++ app/src/main/java/fr/zante/go4lunch/MainActivity.java
@@ -1,11 +1,14 @@
 package fr.zante.go4lunch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -14,6 +17,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import fr.zante.go4lunch.databinding.ActivityMainBinding;
+import fr.zante.go4lunch.ui.restaurantdetail.RestaurantActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = binding.appBarMain.contentMain.bottomNavView;
 
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_lunch, R.id.nav_settings, R.id.nav_logout, R.id.nav_mapview, R.id.nav_listview, R.id.nav_workmates)
+                R.id.nav_settings, R.id.nav_logout, R.id.nav_mapview, R.id.nav_listview, R.id.nav_workmates)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
@@ -42,6 +46,18 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navigationView, navController);
 
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        binding.navView.getMenu().getItem(0).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(@NonNull MenuItem menuItem) {
+                Intent intent = new Intent(getApplicationContext(), RestaurantActivity.class);
+                Bundle myBundle = new Bundle();
+                myBundle.putString("RESTAURANT_PLACE_ID", "ChIJcxi2sojVwkcRGbFnQjPp7xU");
+                intent.putExtra("BUNDLE_RESTAURANT_SELECTED", myBundle);
+                startActivity(intent);
+                return false;
+            }
+        });
 
     }
 
